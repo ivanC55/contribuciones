d0175950ddb456b194e94e3da3247116525e4f62
ivanC55 <dawicerrosb@iescastelar.com>
2023-09-20 19:13:23 +0200

    añado carpetas

---
+++ Proyecto/sumaDos.java
@@ -0,0 +1,8 @@
+public class sumaDos {
+public static void main(String[] args) {
+    int primero = 5;
+    int segundo = 9;
+    int suma = primero + segundo;
+    System.out.println("La suma es : " + suma);
+}
+}
